03dcb86e4f4c52d2a0479ad29b2ef4ae0ad3575a
Chris Shaw <shaw64489@yahoo.com>
2018-11-05 18:55:48 -0500

    finish list functionality and begin likes model

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /**** GLOBAL STATE OF THE APP ****
@@ -11,6 +13,7 @@ import { elements, renderLoader, clearLoader } from './views/base';
 - Liked recipes
 *********************************/
 const state = {};
+window.state = state;
 
 /**
  * SEARCH CONTROLLER
@@ -64,7 +67,7 @@ elements.searchResPages.addEventListener('click', e => {
 const controlRecipe = async () => {
   // Get ID from URL
   const id = window.location.hash.replace('#', '');
-  console.log(id);
+
 
   if (id) {
     // Prepare UI for changes
@@ -101,7 +104,50 @@ window.addEventListener('load', controlRecipe);
 * can use loop for both below
 */
 
+
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+/**
+ * SHOPPING LIST CONTROLLER
+ */
+
+const controlList = () => {
+
+  // Create a new list if there is none yet
+  if (!state.list) state.list = new List();
+
+  // Add each ingredient to the list and UI
+  state.recipe.ingredients.forEach(el => {
+
+    const item = state.list.addItem(el.count, el.unit, el.ingredient);
+    listView.renderItem(item);
+  });
+}
+
+
+// Handle delete and update list item events
+elements.shopping.addEventListener('click', e => {
+
+  const id = e.target.closest('.shopping__item').dataset.itemid;
+
+  // Handle the delete button
+  if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+
+      // Delete from state
+      state.list.deleteItem(id);
+
+      // Delete from UI
+      listView.deleteItem(id);
+
+      // Handle the count update
+  } else if (e.target.matches('.shopping__count-value')) {
+
+    const val = parseFloat(e.target.value, 10);
+    state.list.updateCount(id, val);
+
+  }
+
+});
 
 
 // Handling recipe button clicks
@@ -109,16 +155,22 @@ elements.recipe.addEventListener('click', e => {
   if (e.target.matches('.btn-decrease, .btn-decrease *')) {
     //  Decrease button is clicked
     if (state.recipe.servings > 1) {
-      state.recipe.updatServings('dec');
+      state.recipe.updateServings('dec');
       recipeView.updateServingsIngredients(state.recipe);
     }
 
   } else if (e.target.matches('.btn-increase, .btn-increase *')) {
     //  Increase button is clicked
-    state.recipe.updatServings('inc');
+    state.recipe.updateServings('inc');
     recipeView.updateServingsIngredients(state.recipe);
 
+  } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+    console.log('list add')
+    controlList();
   }
 
 });
+
+
+
 
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -63,12 +63,12 @@ export const renderRecipe = recipe => {
                     <span class="recipe__info-text"> servings</span>
 
                     <div class="recipe__info-buttons">
-                        <button class="btn-tiny">
+                        <button class="btn-tiny btn-decrease">
                             <svg>
                                 <use href="img/icons.svg#icon-circle-with-minus"></use>
                             </svg>
                         </button>
-                        <button class="btn-tiny">
+                        <button class="btn-tiny btn-increase">
                             <svg>
                                 <use href="img/icons.svg#icon-circle-with-plus"></use>
                             </svg>
@@ -93,7 +93,7 @@ export const renderRecipe = recipe => {
      
                 </ul>
 
-                <button class="btn-small recipe__btn">
+                <button class="btn-small recipe__btn recipe__btn--add">
                     <svg class="search__icon">
                         <use href="img/icons.svg#icon-shopping-cart"></use>
                     </svg>
